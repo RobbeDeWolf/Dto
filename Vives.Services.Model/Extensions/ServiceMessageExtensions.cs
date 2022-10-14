9347a3f77685e2d0b7aedfc0af4d06f715cbe786
RobbeDeWolf <99886711+RobbeDeWolf@users.noreply.github.com>
2022-10-14 16:09:19 +0200

    Initial commit

---
+++ Vives.Services.Model/Extensions/ServiceMessageExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace Vives.Services.Model.Extensions
+{
+    public static class ServiceMessageExtensions
+    {
+        public static ServiceResult<T> JsonNull<T>(this ServiceResult<T> serviceResult)
+        {
+            serviceResult.AddJsonNull();
+            return serviceResult;
+        }
+
+        public static ServiceResult JsonNull(this ServiceResult serviceResult)
+        {
+            serviceResult.AddJsonNull();
+            return serviceResult;
+        }
+
+        private static void AddJsonNull(this ServiceResult serviceResult)
+        {
+            serviceResult.Messages.Add(new ServiceMessage
+            {
+                Code = "JsonNullError",
+                Message = "Could not parse json.",
+                Type = ServiceMessageType.Error
+            });
+        }
+    }
+}
